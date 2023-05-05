d8a79982887f9a38e10fb19060bdadb2fc846549
YZYlpk <646100875@qq.com>
2023-05-05 17:20:13 +0800

    阿里云accessKey放泄露

--- src/main/java/com/itheima/utils/SMSUtils.java
+++ src/main/java/com/itheima/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5t8n7MG9N7BJScMgFxv2", "6h5wvE6kLdg6cofVYutiTUc8XlynPA");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
