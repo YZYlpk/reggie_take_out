1ca3cfbdb1dfd87b9d9cbcea0a4f5e2978da693e
YZYlpk <646100875@qq.com>
2023-05-08 11:35:18 +0800

    导入jedis坐标：java中操作Redis

--- src/test/java/com/itheima/ReggieApplicationTest.java
+++ src/test/java/com/itheima/ReggieApplicationTest.java
@@ -3,7 +3,7 @@ package com.itheima;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import redis.clients.jedis.Jedis;
 
 
 /**
@@ -17,5 +17,27 @@ public class ReggieApplicationTest {
         String substring = fileName.substring(fileName.lastIndexOf("."));
         System.out.println(substring);
 
+    }
+
+    /**
+     * 使用jedis操作Redis
+     */
+    @Test
+    public void testRedis(){
+        //先启动redis服务
+        //1.获取连接
+        Jedis jedis=new Jedis("localhost",6379);
+
+        //2.执行具体操作
+        jedis.set("username","xiaoming");
+        String username = jedis.get("username");
+        System.out.println(username);
+
+        jedis.hset("myhash","one","1");
+        String hget = jedis.hget("myhash", "one");
+        System.out.println(hget);
+
+        //3.关闭连接
+        jedis.close();
     }
 }
